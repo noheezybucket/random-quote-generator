b568d63bc68894f01d157d1c02e04c4908fdae85
Mouhamad Gueye <seydinag023@gmail.com>
2022-11-23 17:19:16 +0000

    configuring gh-pages

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import twitter from "../assets/twitter.png";
+import tumblr from "../assets/tumblr.png";
+
+import quotes from "../data/quotes";
+
+import "../styles/QuoteBox.css";
+
+const QuoteBox = () => {
+  const [quote, setQuote] = useState(0);
+
+  const newQuote = (max) => {
+    const random = Math.floor(Math.random() * max);
+    console.log(random);
+    return setQuote(random);
+  };
+
+  return (
+    <div id="quote-box">
+      <div id="text">{quotes[quote].quote}</div>
+      <div id="author">- {quotes[quote].author}</div>
+      <div className="flex-box">
+        <div className="share-box">
+          <a href="twitter.com/intent/tweet" id="tweet-quote">
+            <img src={twitter} alt="twitter" />
+          </a>
+          <a href="twitter.com/intent/tweet" id="tweet-quote">
+            <img src={tumblr} alt="tumblr" />
+          </a>
+        </div>
+        <button onClick={() => newQuote(7)} id="new-quote">
+          New Quote
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QuoteBox;
---
+++ src/data/quotes.js
@@ -0,0 +1,46 @@
+const quotes = [
+  {
+    id: 1,
+    quote:
+      "We cannot solve problems with the kind of thinking we employed when we came up with them.",
+    author: "Albert Einstein",
+  },
+
+  {
+    id: 2,
+    quote:
+      "Learn as if you will live forever, live like you will die tomorrow.",
+    author: "Mahatma Ghandi",
+  },
+  {
+    id: 3,
+    quote: "When you change your thoughts, remember to also change your world.",
+    author: "Norman Vincent Peale",
+  },
+  {
+    id: 4,
+    quote:
+      "It is only when we take chances, when our lives improve. The initial and the most difficult risk that we need to take is to become honest.",
+    author: "Walter Anderson",
+  },
+  {
+    id: 5,
+    quote:
+      "Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.",
+    author: "Diane McLaren",
+  },
+  {
+    id: 6,
+    quote:
+      "Success is getting what you want, happiness is wanting what you get.",
+    author: "W.P Kinsella",
+  },
+  {
+    id: 7,
+    quote:
+      "Nature has given us all the pieces required to achieve exceptional wellness and health, but has left it to us to put these pieces together.",
+    author: "Diane McLaren",
+  },
+];
+
+export default quotes;
