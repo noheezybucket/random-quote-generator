167386022b6e4a3b9fcc1fdc606538625097c276
Mouhamad Gueye <seydinag023@gmail.com>
2022-11-24 17:55:30 +0000

    color switching added

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -2,40 +2,103 @@ import React, { useState } from "react";
 
 import twitter from "../assets/twitter.png";
 import tumblr from "../assets/tumblr.png";
-import quote_left from "../assets/quote.png";
+import quote_left from "../assets/quote.svg";
 
 import quotes from "../data/quotes";
+import colors from "../data/colors";
 
 import "../styles/QuoteBox.css";
 
 const QuoteBox = () => {
   const [quote, setQuote] = useState(0);
+  const [color, setColor] = useState(
+    `#${colors[Math.floor(Math.random() * 3)]}`
+  );
+  console.log(color);
 
-  const newQuote = (max) => {
-    const random = Math.floor(Math.random() * max);
-    console.log(random);
-    return setQuote(random);
+  const newQuote = (max, max2) => {
+    const randomQuote = Math.floor(Math.random() * max);
+    const randomColor = Math.floor(Math.random() * max2);
+
+    setQuote(randomQuote);
+    setColor(`#${colors[randomColor]}`);
+
+    console.log(randomColor);
+    console.log(color);
   };
 
   return (
-    <div id="quote-box">
-      <div id="text">
-        <img src={quote_left} id="quote-left" alt="quote" />{" "}
-        {quotes[quote].quote}
-      </div>
-      <div id="author">- {quotes[quote].author}</div>
-      <div className="flex-box">
-        <div className="share-box">
-          <a href="twitter.com/intent/tweet" id="tweet-quote">
-            <img src={twitter} alt="twitter" />
-          </a>
-          <a href="twitter.com/intent/tweet" id="tweet-quote">
-            <img src={tumblr} alt="tumblr" />
-          </a>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: `${color}`,
+        overflow: "hidden",
+        transition: "0.4s ease-in-out",
+      }}
+    >
+      <div id="quote-box">
+        <div
+          id="text"
+          style={{
+            color: `${color}`,
+            transition: "0.4s ease-in-out",
+          }}
+        >
+          <img
+            src={quote_left}
+            style={{
+              color: `${color}`,
+              transition: "0.4s ease-in-out",
+            }}
+            id="quote-left"
+            alt="quote"
+          />{" "}
+          {quotes[quote].quote}
         </div>
-        <button onClick={() => newQuote(7)} id="new-quote">
-          New Quote
-        </button>
+        <div
+          id="author"
+          style={{
+            color: `${color}`,
+            transition: "0.4s ease-in-out",
+          }}
+        >
+          - {quotes[quote].author}
+        </div>
+        <div className="flex-box">
+          <div className="share-box">
+            <a
+              href="twitter.com/intent/tweet"
+              id="tweet-quote"
+              style={{
+                backgroundColor: `${color}`,
+                transition: "0.4s ease-in-out",
+              }}
+            >
+              <img src={twitter} alt="twitter" />
+            </a>
+            <a
+              href="twitter.com/intent/tweet"
+              id="tweet-quote"
+              style={{
+                backgroundColor: `${color}`,
+                transition: "0.4s ease-in-out",
+              }}
+            >
+              <img src={tumblr} alt="tumblr" />
+            </a>
+          </div>
+          <button
+            style={{ backgroundColor: `${color}` }}
+            onClick={() => newQuote(quotes.length, colors.length)}
+            id="new-quote"
+          >
+            New Quote
+          </button>
+        </div>
       </div>
     </div>
   );
---
+++ src/data/colors.js
@@ -0,0 +1,13 @@
+const colors = [
+  "393E46",
+  "6D9886",
+  "F5D5AE",
+  "EF9A53",
+  "DEBACE",
+  "4E6C50",
+  "0E5E6F",
+  "3A8891",
+  "FF8787",
+];
+
+export default colors;
